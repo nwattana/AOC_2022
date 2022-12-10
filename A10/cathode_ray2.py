cde8ee4b6d254f93e9ea5fb4a8b8976b65d0b568
Narin Wattanasiri <nwattana@z2t1c2.42bangkok.com>
2022-12-10 15:02:09 +0700

    finish day 10

--- A10/cathode_ray2.py
+++ A10/cathode_ray2.py
@@ -6,7 +6,7 @@
 #    By: nwattana <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/12/10 12:38:29 by nwattana          #+#    #+#              #
-#    Updated: 2022/12/10 14:22:30 by nwattana         ###   ########.fr        #
+#    Updated: 2022/12/10 15:01:50 by nwattana         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,11 +17,15 @@ f_obj = f.read().split("\n")
  creat sprit for plot
 """
 def sprit_move(pos):
-	buffer = ['.' for i in range (40)]
+	buffere = ['.' for i in range (40)]
 	for i in range(3):
-		if i + pos < 40:
-			buffer[pos+i] = "#"
-	return (buffer)
+##		print("i + pot => {} + {} = {}".format(i, pos, i + pos))
+		if int(i + pos) < 40 and i + pos > 0:
+##			print(int(i + pos))
+			buffere[pos+i] = "\033[1;31;41m#\033[0m"
+#	buffere[0] = '.' # bug with no idea so put this to make monitor clean
+	print("sprit = "+"".join(buffere))
+	return (buffere)
 
 
 """
@@ -59,6 +63,7 @@ clist_index = 0
 plist=[]
 sp = sprit_move(0)
 
+monitor[cycle // 40][cycle % 40] = sp[cycle % 40]
 for index, command in enumerate(f_obj):
 		if command == "":
 			break
@@ -78,15 +83,10 @@ for index, command in enumerate(f_obj):
 				plist.append(data)
 			data += int(line[1])
 		sp = sprit_move(data)
-
-
-"""
-	Put monitor
-"""
+		
 print("="*40)
 for line in monitor:
-	print("".join(line))
+		print("".join(line))
 print("="*40)
 print("".join(sp))
 print("="*40)
-
